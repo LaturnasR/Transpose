11158a99534beebf17cdbafc3f2511a0a2578e99
LaturnasR <qrdlaturnas@tip.edu.ph>
2023-03-09 21:57:30 +0800

    learn contents, transfer table

--- transpose/website/static/js/translate_post.js
+++ transpose/website/static/js/translate_post.js
@@ -11,6 +11,7 @@ $(document).on('submit','#sentence_form',function(e){
 	})
 	.done(function(response){
 		$("#output").html(response);
+  		MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 	})
 	.fail(function(){
 		$("#output").html("<h6 class='text-danger'>Browser failed to execute the server script</h6>");
